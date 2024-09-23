e88771730d2bc669977f832dc13552d5335c4ead
nrequena <requena.nicolas@gmail.com>
2024-09-23 08:43:32 -0300

    092324-00

--- ambiences.js
+++ ambiences.js
@@ -4,66 +4,75 @@ let audioIsInit = false;
 const now = new Date();
 const reverb = new Tone.Reverb();
 reverb.wet.value = 0.7;
-const lpf = new Tone.Filter(350, 'lowpass');
+const lpf = new Tone.Filter(250, 'lowpass');
 const gainNode = new Tone.Gain(0.7);
 const bpm = 20;
 Tone.Transport.bpm.value = bpm;
 
+const droneArray = []
 
-// const morningDrones = {
-//     Gmaj: './sound/drones/Gmaj_Drone_01.wav',    
-// }
+const morningDrones = {
+    Gmaj1: './sound/drones/Gmaj_Loop_01.wav',    
+    Gmaj2: './sound/drones/Gmaj_Loop_02.wav',    
+    Gmaj3: './sound/drones/Gmaj_Loop_03.wav',    
+}
 
-// const dayDrones = {
-//     Cmaj: './sound/drones/Cmaj_Drone_01.wav',
-// }
-// const afternoonDrones = {
-//     Amin: './sound/drones/Amin_Drone_01.wav',
-// }
+const dayDrones = {
+    Cmaj1: './sound/drones/Cmaj_Loop_1.mp3',
+    Cmaj2: './sound/drones/Cmaj_Loop_2.mp3',
+    Cmaj3: './sound/drones/Cmaj_Loop_3.mp3',
+}
+const afternoonDrones = {
+    Amin1: './sound/drones/Amin_Loop_01.mp3',
+    Amin2: './sound/drones/Amin_Loop_02.mp3',
+    Amin3: './sound/drones/Amin_Loop_03.mp3',
+}
 
-// const nightDrones = {
-//     Dmin: './sound/drones/Dmin_Drone_01.wav',
-// }
+const nightDrones = {
+    Dmin1: './sound/drones/Dmin_Loop_01.mp3',
+    Dmin2: './sound/drones/Dmin_Loop_02.mp3',
+    Dmin3: './sound/drones/Dmin_Loop_03.mp3',
+}
 
-// const dayGrainSynth = new Tone.GrainPlayer({
-//     url: dayDrones.Cmaj,
-//     loop: true,
-//     grainSize: 0.1,
-//     overlap: 0.8,
-//     playbackRate: 1
-// }).connect(reverb);
+const dayGrainSynth = new Tone.GrainPlayer({
+    url: dayDrones.Cmaj1,
+    loop: true,
+    grainSize: 0.1,
+    overlap: 0.8,
+    playbackRate: 1
+}).connect(reverb);
 
-// const nightGrainSynth = new Tone.GrainPlayer({
-//     url: nightDrones.Dmin,
-//     loop: true,
-//     loopStart: 0,
-//     loopEnd: 1,
-//     reverse: true,
-//     grainSize: 0.2,
-//     overlap: 0.05,
-//     playbackRate: 1
-// }).connect(reverb);
+const nightGrainSynth = new Tone.GrainPlayer({
+    url: nightDrones.Dmin1,
+    loop: true,
+    loopStart: 0,
+    loopEnd: 1,
+    reverse: false,
+    grainSize: 0.2,
+    overlap: 0.05,
+    playbackRate: 1
+}).connect(reverb);
 
-// const afternoonGrainSynth = new Tone.GrainPlayer({
-//     url: afternoonDrones.Amin,
-//     loop: true,
-//     grainSize: 0.5,
-//     overlap: 0.5,
-//     playbackRate: 0.1
-// }).connect(reverb);
+const afternoonGrainSynth = new Tone.GrainPlayer({
+    url: afternoonDrones.Amin1,
+    loop: true,
+    grainSize: 0.1,
+    overlap: 0.3,
+    playbackRate: 0.5
+}).connect(reverb);
 
-// const morningGrainSynth = new Tone.GrainPlayer({
-//     url: morningDrones.Gmaj,
-//     loop: true,
-//     grainSize: 0.5,
-//     overlap: 0.5,
-//     playbackRate: 0.1
-// }).connect(reverb);
+const morningGrainSynth = new Tone.GrainPlayer({
+    url: morningDrones.Gmaj1,
+    loop: true,
+    grainSize: 0.5,
+    overlap: 0.5,
+    playbackRate: 0.1
+}).connect(reverb);
 
 // Define some chords
 const chords = {
     Dmin: ["D1", "F2", "A3", "C3"],
-    Amin: ["C2", "A2", "E3", "F3"],
+    Amin: ['A1', 'A2', 'E3', 'F3'],
     Gmaj: ["G1", "D2", "A2", "B2"],
     Cmaj: ["C1", "C2", "G2", "F3"]
 };
@@ -82,8 +91,25 @@ const padSynth = new Tone.PolySynth(Tone.Synth, {
     },
 }).connect(reverb);
 
+const subSynth = new Tone.Synth({
+    oscillator: {
+        type: 'sine',
+      },
+      envelope: {
+        attack: 0.8,
+        decay: 0.6,
+        sustain: 0.6,
+        release: 1,
+      },
+}).connect(reverb);
 
-
+const afternoonSeq = new Tone.Sequence(
+    (time, note) => {
+      subSynth.triggerAttackRelease(note, '1n', time);
+    },
+    ['D1'],
+    '1n'
+    );
 
 const samples = {
     morning: {
@@ -125,7 +151,6 @@ function startSoundscape() {
                 case 'Sunny':
                     //playSample('morning', 'sunny');
                     console.log('Sunny');
-                    padSynth.triggerAttackRelease(chords.Dmin, "1n");
                     break;
                 case 'Cloudy':
                     //playSample('morning', 'cloudy');  
@@ -152,8 +177,11 @@ function startSoundscape() {
         } else if (timeOfDay > 14 && timeOfDay <= 20) {
             console.log('Afternoon');
             switch(condition) {
-                case 'Sunny':
+                case 'Clear':
                     //playSample('afternoon', 'sunny');
+                    //afternoonGrainSynth.start();
+                    //afternoonSeq.start(0);
+                    subSynth.triggerAttackRelease('D1', "2m");
                     console.log('Sunny');
                     break;
                 case 'Cloudy':
@@ -168,6 +196,7 @@ function startSoundscape() {
             switch(condition) {
                 case 'Clear':
                     //playSample('night', 'sunny');
+                    nightGrainSynth.start();
                     console.log('Clear');
                     break;
                 case 'Cloudy':
@@ -192,7 +221,6 @@ document.getElementById('initAudio').addEventListener('click', async () => {
           console.log('STARTED SOUNDSCAPE');
       } catch (error) {
           console.log('Error:', error);
-          granularSynth.stop();
       }
   } else {
       Tone.Transport.stop();
