395886b97eac66d375296fa5da5b51f28afb339e
Nicolas Requena <58339988+NRequena@users.noreply.github.com>
2025-01-15 10:28:44 -0300

    portfolio-06

--- src/pages/projects/TheNoise.tsx
+++ src/pages/projects/TheNoise.tsx
@@ -21,27 +21,34 @@ const TheNoise = () => {
               }}
             />
                 <h2 className="w-full font-light py-4">
-                     The Concept   
+                     Researing and falling deep on a Close Encounters, Random Event Generators, UFOs and Liminal spaces rabbit hole I came up with and idea for a story (Actually it was a video game first).
+                     A world, characters, events, interactions and lore for a world in which my story develops.
+                     I don't like to think of it as a Film, Book or Game...it's an idea, a SPACE, and I'd like to explore it and see what outputs I can get from it. Go deep into it and see what I can come out with.
+
+                     Concept work will be feautred here as well as the process of using this creative space to generate Art, working towards making a Short Horror First Person Retro Game and a Short Film.
+
+                     I like the idea of creating things that may exist in the universe that I'm writing about. For that I'm also prototyping some Electronic Devices that generate Sounds with AI and data from weird events 
+
                 </h2>
                 <h3 className="text-sm">
-                     Spotify Playlist - The Music
+                     Moody Playlist
                     </h3>
                     <iframe className="md:flex" src="https://open.spotify.com/embed/playlist/3S0sljsAvx3rlWQlZJDkag?utm_source=generator&theme=0" width="50%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <h2 className="w-full font-light py-4">
-                 Images
+                 Concept Images
                 </h2>
                 <h2 className="w-full font-light py-4">
-                 The Film
+                 Short Film Clips (WIP)
                 </h2>
                 <h2 className="w-full font-light py-4">
                  The Story
                 </h2>
                 <h2 className="w-full font-light py-4">
-                 Design and Generate with AI
-                 <h3 className="text-sm">ComfyUI</h3>
+                 Design and Generate with AI - Storyboard
+                </h2>
+                <h3 className="text-sm">ComfyUI - FLUX</h3>
                  <h3 className="text-sm">Text2Speech</h3>
                  <h3 className="text-sm">PyramidFlow, Runway, LTX</h3>
-                </h2>
                 <h2 className="w-full font-light py-4">
                  The Game
                 </h2>
--- src/pages/projects/TheTape.tsx
+++ src/pages/projects/TheTape.tsx
@@ -5,26 +5,37 @@ export default function TheTape() {
          BANNER
         </h2>
       <h1 className="text-2xl text-semibold text-gray-800 font-bold">
-        The Tape - Concept Unreal Engine Short Film
+        The Tape (2021) - Unreal Engine Short Film
       </h1>
       <div>
           <h3 className="w-full font-extralight text-sm py-1 text-nasared">
-          #unrealengine #vfx #blender #music
+          #unrealengine #vfx #blender #music #rendering #modeling
           </h3>
         <h2 className="w-full font-light py-4">
-          Short Film
+          <p>The tape is a short concept film to learn Unreal Engine, sing Lumen and rendering in real time.</p>
+          <br/>
+          <p>I wanted to focus on light, rendering and getting a simple story and short film from a few assets from the Unreal Library.</p> 
+          <br/>
+          <p>I always make original music and sound design for my projects and this was not the exception, I love the last part of the process of making a film, getting to add my music and SFX always gets me into a flow state.</p>
+          <br/>
         </h2>
         <h2 className="w-full font-light py-4">
-          Stills
+          STILLS
         </h2>        
         <h2 className="w-full font-light py-4">
           Unreal Engine
         </h2>
         <h2 className="w-full font-light py-4">
-          Story
+          The idea came from listening to some old EVP (Electronic Voice Phenomenon) recordings. The idea of a old cassette recording that someone can disect and recover to find strange and paranormal sounds.
         </h2>
         <h2 className="w-full font-light py-4">
-          Gameplay
+          SHORT FILM EMBEDED
+        </h2>
+        <h2 className="w-full font-light py-4">
+          OST
+        </h2>
+        <h2>
+          Redes, Perfiles, Etc.
         </h2>
       </div>
     </div>
